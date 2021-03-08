b91120a666bebaa506bcc105621b0fb0df7fc2dc
Bo Qing <bqing1@yahoo.com>
2021-03-07 18:50:24 -0800

    Implemented #23 & 24

--- src/recursion.js
+++ src/recursion.js
@@ -248,11 +248,36 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  var result = 0;
+  for (var key in obj) {
+    var v = obj[key];
+    if (v === value) {
+      result++;
+    }
+    if (typeof v === 'object') {
+      result += countValuesInObj(v, value);
+    }
+  }
+
+  return result;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+  for (var key in obj) {
+    if (key === oldKey) {
+      obj[newKey] = obj[key];
+      delete obj[key];
+    }
+
+    var value = obj[key];
+    if (typeof value === 'object') {
+      replaceKeysInObj(value, oldKey, newKey);
+    }
+  }
+
+  return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
